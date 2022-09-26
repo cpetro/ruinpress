f11fba1a0ed01c795148aca6f9257c83a3fe3d57
petro <info@chrispetro.net>
2022-09-26 10:12:51 +1000

    no message

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
 import { PayPalScriptProvider } from "@paypal/react-paypal-js";
 
